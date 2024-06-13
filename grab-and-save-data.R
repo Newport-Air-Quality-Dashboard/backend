4ac18721b711edab5c69b935c590bb4e0c2dcefe
tylernku <massiet1@mymail.nku>
2024-06-12 20:22:04 -0400

    fixed issue where trying to rename fields that didn't exist would crash the program

--- grab-and-save-data.R
+++ grab-and-save-data.R
@@ -1,20 +1,25 @@
 #!/usr/bin/env Rscript
 # install.packages(c("httr", "jsonlite", "tidyverse"))
 
-# library(con2aqi)
+library(con2aqi)
 library(httr)
 library(jsonlite)
 library(tidyverse)
 
+# I decided it was a bad idea to have api keys in a repo so i've used "config.R"
+# to store sensitive data. just copy the config.R.example to config.R.
 source("config.R")
 
-# Define the API key
-get_EPA_data <- function(nwlng, nwlat,  selng, selat,
-                         data_type = "B", monitor_type = "2", verbose = "1",
-                         parameters = c("OZONE", "PM25", "PM10", "CO", "NO2", "SO2"),
-                         include_raw_concentrations = "1", api_key,
-                         start_date, end_date) {
+# Function to pull data from the EPA's AirNow EPA. Requires an AirNow account
+# and an API key. 
+get_epa <- function(nwlng, nwlat,  selng, selat,
+                    data_type = "B", monitor_type = "2", verbose = "1",
+                    parameters = c("OZONE", "PM25", "PM10", "CO", "NO2", "SO2"),
+                    include_raw_concentrations = "1", api_key,
+                    start_date, end_date) {
 
+  # epa allows you to select an area for sensors. this creates that from 2 lats
+  # and 2 longs
   bbox <- paste(nwlng, nwlat, selng, selat, sep=",")
 
   root_url <- "https://www.airnowapi.org/aq/data/?"
@@ -28,16 +33,17 @@ get_EPA_data <- function(nwlng, nwlat,  selng, selat,
                          "&includerawconcentrations=", include_raw_concentrations,
                          "&API_KEY=", api_key)
 
+  # if you specify start_date and end_date, it'll pull data for that entire
+  # period. by default it will pull the most recent hour.
   if(!missing(start_date) && !missing(end_date)) {
     parameter_url <- paste0(parameter_url, 
                             "&startDate=", start_date,
                             "&endDate=", end_date)
   }
   
+  # build the final url to request from
   api_url <- paste0(root_url, parameter_url)
   
-  print(api_url)
-
   response <- GET(api_url)
   
   if (status_code(response) == 200) {
@@ -53,55 +59,78 @@ get_EPA_data <- function(nwlng, nwlat,  selng, selat,
   return (df)
 }
 
+# Transforms the EPA data from long to wide format and renames the columns to 
+# be synonymous with PurpleAir's data. 
 transform_epa <- function(df) {
   
-    df$aqs_id <- NULL 
-    
-    df <- df %>% pivot_wider(names_from=parameter, values_from=c(concentration, aqi, category, raw_concentration, unit)) 
+  # aqs_id is redundant. there's another field called "full aqs id" that
+  # contains the aqs_id and also the country code
+  df$aqs_id <- NULL 
   
-    df <- df %>% rename(pm2.5_aqi = aqi_PM2.5,
-                        no2_aqi = aqi_NO2,
-                        co_aqi = aqi_CO,
-                        ozone_aqi = aqi_OZONE,
-                        pm10.0_aqi = aqi_PM10,
-                        so2_aqi = aqi_SO2,
-                        pm2.5_category = category_PM2.5, 
-                        no2_category = category_NO2,
-                        co_category = category_CO,
-                        ozone_category = category_OZONE,
-                        pm10.0_category = category_PM10,
-                        so2_category = category_SO2,
-                        pm2.5_unit = unit_PM2.5, 
-                        no2_unit = unit_NO2,
-                        co_unit = unit_CO,
-                        ozone_unit = unit_OZONE,
-                        pm10.0_unit = unit_PM10,
-                        so2_unit = unit_SO2,
-                        pm2.5_60minute = concentration_PM2.5, 
-                        no2_60minute = concentration_NO2,
-                        co_60minute = concentration_CO,
-                        ozone_60minute = concentration_OZONE,
-                        pm10.0_60minute = concentration_PM10,
-                        so2_60minute = concentration_SO2,
-                        pm2.5_atm = raw_concentration_PM2.5, 
-                        no2_atm = raw_concentration_NO2,
-                        co_atm = raw_concentration_CO,
-                        ozone_atm = raw_concentration_OZONE,
-                        pm10.0_atm = raw_concentration_PM10,
-                        so2_atm = raw_concentration_SO2)
+  # epa uses -999 to signify empty values. I've also seen -999.0 so replace
+  # both to be safe.
+  df[df == -999] <- NA
+  df[df == -999.0] <- NA
     
-    df[, c('no2_60minute', 'co_60minute', 'so2_60minute')] <- list(NULL)
-    
-    df$time_stamp <- as.POSIXct(df$time_stamp,
-                                format = "%Y-%m-%dT%H:%M", tz="UTC") 
-    df$sensor_index <- as.character(df$sensor_index)
-    df$source <- "EPA"
-    
-    return(df)
+  # converts the epa data from long to wide form.
+  df <- df %>% pivot_wider(names_from=parameter, values_from=c(concentration, aqi, category, raw_concentration, unit)) 
+  
+  # vector used to rename the epa fields to match purple air.
+  to_rename <- c(pm2.5_aqi = "aqi_PM2.5",
+                 no2_aqi = "aqi_NO2",
+                 co_aqi = "aqi_CO",
+                 ozone_aqi = "aqi_OZONE",
+                 pm10.0_aqi = "aqi_PM10",
+                 so2_aqi = "aqi_SO2",
+                 pm2.5_category = "category_PM2.5", 
+                 no2_category = "category_NO2",
+                 co_category = "category_CO",
+                 ozone_category = "category_OZONE",
+                 pm10.0_category = "category_PM10",
+                 so2_category = "category_SO2",
+                 pm2.5_unit = "unit_PM2.5", 
+                 no2_unit = "unit_NO2",
+                 co_unit = "unit_CO",
+                 ozone_unit = "unit_OZONE",
+                 pm10.0_unit = "unit_PM10",
+                 so2_unit = "unit_SO2",
+                 pm2.5_60minute = "concentration_PM2.5", 
+                 no2_60minute = "concentration_NO2",
+                 co_60minute = "concentration_CO",
+                 ozone_60minute = "concentration_OZONE",
+                 pm10.0_60minute = "concentration_PM10",
+                 so2_60minute = "concentration_SO2",
+                 pm2.5_atm = "raw_concentration_PM2.5", 
+                 no2_atm = "raw_concentration_NO2",
+                 co_atm = "raw_concentration_CO",
+                 ozone_atm = "raw_concentration_OZONE",
+                 pm10.0_atm = "raw_concentration_PM10",
+                 so2_atm = "raw_concentration_SO2")
+  
+  # originally, i had issues with the program erroring and crashing if a field
+  # was absent that was trying to be renamed. using any_of and a character
+  # vector fixed that.
+  df <- df %>% rename(any_of(to_rename))
+  
+  # no2, co, and so2 have the same values in "concentration so i remove these 
+  # fields to prevent misunderstandings of the data.
+  df[, c('no2_60minute', 'co_60minute', 'so2_60minute')] <- list(NULL)
+  
+  # time_stamp is datetime
+  df$time_stamp <- as.POSIXct(df$time_stamp,
+                              format = "%Y-%m-%dT%H:%M", tz="UTC") 
+  
+  # probably doesn't matter, but makes sure the sensor_index displays as it should
+  df$sensor_index <- as.character(df$sensor_index)
+  
+  df$source <- "EPA"
+  
+  return(df)
 }
 
 
-get_PA_data <- function(nwlng, nwlat, selng, selat, location, api_key) {
+# grabs pa data from the purpleair api. requires an api key with points.
+get_pa <- function(nwlng, nwlat, selng, selat, location, api_key) {
   # PurpleAir API URL
   root_url <- "https://api.purpleair.com/v1/sensors/"
   
@@ -110,7 +139,18 @@ get_PA_data <- function(nwlng, nwlat, selng, selat, location, api_key) {
   ll_api_url <- paste0("&nwlng=", lat_lon[1], "&nwlat=", lat_lon[2], "&selng=", lat_lon[3], "&selat=", lat_lon[4])
   
   # Fields to get
-  fields_list <- c("sensor_index", "last_seen", "name", "latitude", "longitude", "humidity", "temperature", "pressure", "pm1.0", "pm2.5", "pm2.5_10minute", "pm10.0_atm", "confidence")
+  fields_list <- c("sensor_index", 
+                   "last_seen", 
+                   "name", 
+                   "latitude", 
+                   "longitude", 
+                   "humidity", 
+                   "temperature", 
+                   "pressure", 
+                   "pm1.0", 
+                   "pm2.5_atm", "pm2.5_10minute", 
+                   "pm10.0_atm", "confidence")
+  
   fields_api_url <- paste0("&fields=", paste(fields_list, collapse = "%2C"))
   
   # Final API URL
@@ -131,14 +171,18 @@ get_PA_data <- function(nwlng, nwlat, selng, selat, location, api_key) {
   return(df)
 }
 
-transform_purpleair <- function(df) {
+transform_pa <- function(df) {
+  
+  # last_seen reports the time and data associated with that time
   df <- df %>% rename(time_stamp = last_seen)
+  
+  # null is how PA signifies missing data
+  df[df == "null"] <- NA
+  
   df$time_stamp <- as.POSIXct(df$time_stamp, 
                               format = "%s",
                               tz = "UTC") 
   df$sensor_index <- as.character(df$sensor_index)
-  
-  # is.na(df) <- df == "null"
   
   df <- df %>% mutate_at(c('latitude', 
                            'longitude',
@@ -146,15 +190,29 @@ transform_purpleair <- function(df) {
                            'temperature',
                            'pressure',
                            'pm1.0',
-                           'pm2.5',
+                           'pm2.5_atm',
                            'pm2.5_10minute',
                            'pm10.0_atm',
                            'confidence'), as.numeric)  
   
-  df$source <- "PurpleAir"
-  
+  # I've chosen to remove all values that are above these. The EPA says that 
+  # AQI goes from 0 to 500. These values are 500 AQI for their respective 
+  # pollutant. Also con2aqi won't work on values above these so seems like a 
+  # reasonable upper-limit.
+  #
+  # pm2.5  - 500.4
+  # pm10.0 - 604
+  # co     - 50.4
+  # so2    - 1004
+  # no2    - 2049
+  # df["pm2.5_atm"][df["pm2.5_atm" > 500.4]] <- NA
+  # df["pm2.5_10minute"][df["pm2.5_10minute" > 500.4]] <- NA
+  # df["pm10.0"][df["pm10.0" > 604]] <- NA
+  # 
   # df$pm2.5_aqi <- con2aqi("pm25", df$pm2.5_10minute)
   # df$pm10.0_aqi <- con2aqi("pm10", df$pm10.0_atm)
+  
+  df$source <- "PurpleAir"
   
   return(df)
 }
@@ -173,29 +231,33 @@ load(input_df)
 epa_time <- 1
 while (T) {
   print("grabbing PurpleAir data")
-  PA_data <- get_PA_data(-84.534, 39.106, -84.455, 39.050, location, api_key=pa_key)
+  df_pa <- get_pa(-84.534, 39.106, -84.455, 39.050, location, api_key=pa_key)
   print("transforming PurpleAir data")
-  PA_data <- transform_purpleair(PA_data)
+  df_pa <- transform_pa(df_pa)
   
   if (epa_time %% 6 == 0) {
     print("grabbing EPA data")
-    EPA_data <- get_EPA_data(nwlng = nwlng, nwlat = nwlat, selng = selng, selat = selat,
+    df_epa <- get_epa(nwlng = nwlng, nwlat = nwlat, selng = selng, selat = selat,
                              api_key = epa_key)
     print("transforming EPA data")
-    EPA_data <- transform_epa(EPA_data) 
+    df_epa <- transform_epa(df_epa) 
     
     print("joining PurpleAir and EPA data")
-    new_data <- list(PA_data, EPA_data) %>% reduce(full_join, by=c('sensor_index', 'time_stamp', 'latitude',
-                                                                   'longitude', 'name', 'source'))
+    df_new <- list(df_pa, df_epa) %>% reduce(full_join, by=c('sensor_index', 
+                                                             'time_stamp', 
+                                                             'latitude', 
+                                                             'longitude', 
+                                                             'name', 
+                                                             'source'))
     epa_time <- 1
   } else {
-    new_data <- PA_data
+    df_new <- df_pa
   }
   
-  new_data$type <- "real-time"
+  df_new$type <- "real-time"
   
   print("merging real-time with historical data")
-  combined_data <- bind_rows(combined_data, new_data)
+  combined_data <- bind_rows(combined_data, df_new)
   
   print("removing duplicate rows")
   combined_data <- combined_data %>% distinct()
@@ -209,5 +271,3 @@ while (T) {
   Sys.sleep(600) # Sleep 10m
 }
 
-# purpleair_data$pm2.5_aqi <- con2aqi("pm25", purpleair_data$pm2.5_60minute)
-# purpleair_data$pm10.0_aqi <- con2aqi("pm10", purpleair_data$pm10.0_60minute)
