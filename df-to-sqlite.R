27a7165bb65f4c3563807bb959ee3ae84a19030a
tylernku <massiet1@mymail.nku>
2024-07-26 10:46:30 -0400

    documentation

--- df-to-sqlite.R
+++ df-to-sqlite.R
@@ -1,11 +1,24 @@
 #!/usr/bin/env Rscript
 
+# Description: this script just converts from an Rda file to an sqlite database.
+# It's pretty useful because Rda is compressed enough to be committed to github,
+# while the database isn't.
+
+# WARNING: it's set to overwrite whatever table it's set to write to.
+
+my_packages <- c("DBI",
+                 "RSQLite")
+
+not_installed <- my_packages[!(my_packages %in% installed.packages()[ , "Package"])]
+if(length(not_installed)) install.packages(not_installed)    
+
 library(DBI)
 
-db_path <- "./out/df.sqlite"
+db_path <- "./out/db.sqlite"
 df_path <- "./out/df_all.Rda"
 
 load(df_path)
+dbDisconnect(con)
+con <- dbConnect(RSQLite::SQLite(), db_path)
 
-con <- dbConnect(RSQLite::SQLite(), "./out/db.sqlite")
 dbWriteTable(con, "df_all", df_all, overwrite=T)
